63574e747112fff4c9d5c1a2ddc28a0e3b2e9dbe
Ulez <1104128773@qq.com>
2020-11-21 16:30:23 +0800

    点击生成native crash输出到文件目录下

--- app/src/main/java/fun/learnlife/breakpaddemo/MainActivity.java
+++ app/src/main/java/fun/learnlife/breakpaddemo/MainActivity.java
@@ -1,12 +1,24 @@
 package fun.learnlife.breakpaddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.sample.breakpad.BreakpadInit;
+
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100;
+    private File externalReportPath;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -18,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        } else {
+            initExternalReportPath();
+        }
+        initBreakPad();
         // Example of a call to a native method
         TextView tv = findViewById(R.id.text);
         tv.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +48,32 @@ public class MainActivity extends AppCompatActivity {
                 crash();
             }
         });
+    }
+
+    private void initBreakPad() {
+        if (externalReportPath == null) {
+            externalReportPath = new File(getFilesDir(), "crashDump");
+            if (!externalReportPath.exists()) {
+                externalReportPath.mkdirs();
+            }
+        }
+        String path = externalReportPath.getAbsolutePath();
+        Log.i("lcyy", path);
+        BreakpadInit.initBreakpad(path);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        initExternalReportPath();
+    }
+
+    private void initExternalReportPath() {
+        externalReportPath = new File(Environment.getExternalStorageDirectory(), "crashDump");
+        if (!externalReportPath.exists()) {
+            externalReportPath.mkdirs();
+        }
     }
 
     /**
