7fd43da23762ea6ce83f7ce8d354fab0b47157a4
Ulez <1104128773@qq.com>
2020-11-21 16:14:47 +0800

    重命名为crash-lib并且调用。

--- app/src/main/java/fun/learnlife/breakpaddemo/MainActivity.java
+++ app/src/main/java/fun/learnlife/breakpaddemo/MainActivity.java
@@ -3,13 +3,14 @@ package fun.learnlife.breakpaddemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("crash-lib");
     }
 
     @Override
@@ -18,13 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        TextView tv = findViewById(R.id.text);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crash();
+            }
+        });
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void crash();
 }
